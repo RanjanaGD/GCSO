750193937c20df86ffbe1d8b576f21f1bfea729b
RanjanaGD <69150679+RanjanaGD@users.noreply.github.com>
2020-08-20 21:48:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function draw() {
 
   if(wall.x-car.x<(car.width+wall.width)/2){
 
-    //car.velocityX=0;
+    car.velocityX=0;
      var deformation=0.5*weight*speed*speed/22509;
     
     if(deformation>180){
